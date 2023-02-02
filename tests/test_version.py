59f5a6078c3f9ef888cdc58c12ff5d62899077bb
Timothée Mazzucotelli <pawamoy@pm.me>
2023-02-02 18:23:32 +0100

    tests: Parametrize version bumping tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,79 +1,92 @@
 """Test version bumping."""
 
+import pytest
+
 from git_changelog.build import bump
 
 
-def test_bump():  # noqa: WPS218 (too many assert statements)
-    """Test default version bumping."""
-    assert bump("0.0.1") == "0.0.2"
-    assert bump("0.1.0") == "0.1.1"
-    assert bump("0.1.1") == "0.1.2"
-    assert bump("1.0.0") == "1.0.1"
-    assert bump("1.0.1") == "1.0.2"
-    assert bump("1.1.0") == "1.1.1"
-    assert bump("1.1.1") == "1.1.2"
+@pytest.mark.parametrize(
+    ("version", "bumped"),
+    [
+        ("0.0.1", "0.0.2"),
+        ("0.1.0", "0.1.1"),
+        ("0.1.1", "0.1.2"),
+        ("1.0.0", "1.0.1"),
+        ("1.0.1", "1.0.2"),
+        ("1.1.0", "1.1.1"),
+        ("1.1.1", "1.1.2"),
+        ("v0.0.1", "v0.0.2"),
+        ("v0.1.0", "v0.1.1"),
+        ("v0.1.1", "v0.1.2"),
+        ("v1.0.0", "v1.0.1"),
+        ("v1.0.1", "v1.0.2"),
+        ("v1.1.0", "v1.1.1"),
+        ("v1.1.1", "v1.1.2"),
+    ],
+)
+def test_bump_patch(version, bumped):
+    """Test default and patch version bumping.
 
-    assert bump("v0.0.1") == "v0.0.2"
-    assert bump("v0.1.0") == "v0.1.1"
-    assert bump("v0.1.1") == "v0.1.2"
-    assert bump("v1.0.0") == "v1.0.1"
-    assert bump("v1.0.1") == "v1.0.2"
-    assert bump("v1.1.0") == "v1.1.1"
-    assert bump("v1.1.1") == "v1.1.2"
-
-
-def test_bump_patch():  # noqa: WPS218 (too many assert statements)
-    """Test patch version bumping."""
-    assert bump("0.0.1", "patch") == "0.0.2"
-    assert bump("0.1.0", "patch") == "0.1.1"
-    assert bump("0.1.1", "patch") == "0.1.2"
-    assert bump("1.0.0", "patch") == "1.0.1"
-    assert bump("1.0.1", "patch") == "1.0.2"
-    assert bump("1.1.0", "patch") == "1.1.1"
-    assert bump("1.1.1", "patch") == "1.1.2"
-
-    assert bump("v0.0.1", "patch") == "v0.0.2"
-    assert bump("v0.1.0", "patch") == "v0.1.1"
-    assert bump("v0.1.1", "patch") == "v0.1.2"
-    assert bump("v1.0.0", "patch") == "v1.0.1"
-    assert bump("v1.0.1", "patch") == "v1.0.2"
-    assert bump("v1.1.0", "patch") == "v1.1.1"
-    assert bump("v1.1.1", "patch") == "v1.1.2"
+    Parameters:
+        version: The base version.
+        bumped: The expected, bumped version.
+    """
+    assert bump(version) == bump(version, "patch") == bumped
 
 
-def test_bump_minor():  # noqa: WPS218 (too many assert statements)
-    """Test minor version bumping."""
-    assert bump("0.0.1", "minor") == "0.1.0"
-    assert bump("0.1.0", "minor") == "0.2.0"
-    assert bump("0.1.1", "minor") == "0.2.0"
-    assert bump("1.0.0", "minor") == "1.1.0"
-    assert bump("1.0.1", "minor") == "1.1.0"
-    assert bump("1.1.0", "minor") == "1.2.0"
-    assert bump("1.1.1", "minor") == "1.2.0"
+@pytest.mark.parametrize(
+    ("version", "bumped"),
+    [
+        ("0.0.1", "0.1.0"),
+        ("0.1.0", "0.2.0"),
+        ("0.1.1", "0.2.0"),
+        ("1.0.0", "1.1.0"),
+        ("1.0.1", "1.1.0"),
+        ("1.1.0", "1.2.0"),
+        ("1.1.1", "1.2.0"),
+        ("v0.0.1", "v0.1.0"),
+        ("v0.1.0", "v0.2.0"),
+        ("v0.1.1", "v0.2.0"),
+        ("v1.0.0", "v1.1.0"),
+        ("v1.0.1", "v1.1.0"),
+        ("v1.1.0", "v1.2.0"),
+        ("v1.1.1", "v1.2.0"),
+    ],
+)
+def test_bump_minor(version, bumped):
+    """Test minor version bumping.
 
-    assert bump("v0.0.1", "minor") == "v0.1.0"
-    assert bump("v0.1.0", "minor") == "v0.2.0"
-    assert bump("v0.1.1", "minor") == "v0.2.0"
-    assert bump("v1.0.0", "minor") == "v1.1.0"
-    assert bump("v1.0.1", "minor") == "v1.1.0"
-    assert bump("v1.1.0", "minor") == "v1.2.0"
-    assert bump("v1.1.1", "minor") == "v1.2.0"
+    Parameters:
+        version: The base version.
+        bumped: The expected, bumped version.
+    """
+    assert bump(version, "minor") == bumped
 
 
-def test_bump_major():  # noqa: WPS218 (too many assert statements)
-    """Test major version bumping."""
-    assert bump("0.0.1", "major") == "0.1.0"
-    assert bump("0.1.0", "major") == "0.2.0"
-    assert bump("0.1.1", "major") == "0.2.0"
-    assert bump("1.0.0", "major") == "2.0.0"
-    assert bump("1.0.1", "major") == "2.0.0"
-    assert bump("1.1.0", "major") == "2.0.0"
-    assert bump("1.1.1", "major") == "2.0.0"
+@pytest.mark.parametrize(
+    ("version", "bumped"),
+    [
+        ("0.0.1", "0.1.0"),
+        ("0.1.0", "0.2.0"),
+        ("0.1.1", "0.2.0"),
+        ("1.0.0", "2.0.0"),
+        ("1.0.1", "2.0.0"),
+        ("1.1.0", "2.0.0"),
+        ("1.1.1", "2.0.0"),
+        ("v0.0.1", "v0.1.0"),
+        ("v0.1.0", "v0.2.0"),
+        ("v0.1.1", "v0.2.0"),
+        ("v1.0.0", "v2.0.0"),
+        ("v1.0.1", "v2.0.0"),
+        ("v1.1.0", "v2.0.0"),
+        ("v1.1.1", "v2.0.0"),
+    ],
+)
+def test_bump_major(version, bumped):
+    """Test major version bumping.
 
-    assert bump("v0.0.1", "major") == "v0.1.0"
-    assert bump("v0.1.0", "major") == "v0.2.0"
-    assert bump("v0.1.1", "major") == "v0.2.0"
-    assert bump("v1.0.0", "major") == "v2.0.0"
-    assert bump("v1.0.1", "major") == "v2.0.0"
-    assert bump("v1.1.0", "major") == "v2.0.0"
-    assert bump("v1.1.1", "major") == "v2.0.0"
+    Parameters:
+        version: The base version.
+        bumped: The expected, bumped version.
+    """
+    assert bump(version, "major") == bumped
