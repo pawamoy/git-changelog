d3dca0582cb64ffe04f42cddfbca0a7e9a8e2fc3
Timothée Mazzucotelli <pawamoy@pm.me>
2023-02-02 18:18:58 +0100

    refactor: Expose `Changelog` and `Commit` from `git_changelog`

--- src/git_changelog/__init__.py
+++ src/git_changelog/__init__.py
@@ -6,6 +6,6 @@ Automatic Changelog generator using Jinja2 templates.
 
 from __future__ import annotations
 
-from git_changelog.build import GitHub, GitLab
+from git_changelog.build import Changelog, Commit, GitHub, GitLab
 
-__all__: list[str] = ["GitHub", "GitLab"]  # noqa: WPS410 (the only __variable__ we use)
+__all__: list[str] = ["Changelog", "Commit", "GitHub", "GitLab"]  # noqa: WPS410
