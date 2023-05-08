f1060adc40962c5302edbbe82042212af79ffa44
Kevin Squire <kevin@secondspectrum.com>
2023-05-08 15:22:32 -0700

    fix: Check if the latest version tag is already part of the changelog
    Previously, if git-changelog was run twice with bump_latest=True and
    without tagging a new version in between, the full changelog would
    be duplicated and written out.  This is because the latest version
    in the CHANGELOG would not match any existing version tags, so no
    lines would be filtered out before inserting the changelog into the file.

--- src/git_changelog/cli.py
+++ src/git_changelog/cli.py
@@ -350,6 +350,10 @@ def build_and_render(
         # only keep new entries (missing from changelog)
         last_released = _latest(lines, re.compile(version_regex))
         if last_released:
+            # check if the latest version is already in the changelog
+            if (last_released == changelog.versions_list[0].tag or
+                last_released == changelog.versions_list[0].planned_tag):
+                raise ValueError(f"Version {last_released} already in changelog")
             changelog.versions_list = _unreleased(
                 changelog.versions_list,
                 last_released,
