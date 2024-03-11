8e1bac6afd4b8b4065ea0c141dc1cc7a896391dc
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-03-11 12:44:38 +0100

    Update src/git_changelog/build.py

--- src/git_changelog/build.py
+++ src/git_changelog/build.py
@@ -144,12 +144,12 @@ class Version:
         """
         self.commits.append(commit)
         commit.version = self.tag
-        _type = commit.convention["type"]
-        if "type" in commit.convention and _type not in self.sections_dict:
-            section = Section(section_type=_type)
+        commit_type = commit.convention.get("type")
+        if commit_type not in self.sections_dict:
+            section = Section(section_type=commit_type)
             self.sections_list.append(section)
-            self.sections_dict[_type] = section
-        self.sections_dict[_type].commits.append(commit)
+            self.sections_dict[commit_type] = section
+        self.sections_dict[commit_type].commits.append(commit)
 
 
 class Changelog:
